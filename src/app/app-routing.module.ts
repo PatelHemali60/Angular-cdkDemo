138599e57ee8ae100204756e74bab763783155e2
Hemali Patel <hemali.patel@1Rintern.local>
2022-03-11 18:55:05 +0530

    feat:AnuglarCdk demo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgtemplateDemoComponent } from './components/ngtemplate-demo/ngtemplate-demo.component';
+
+const routes: Routes = [
+  {
+    path:'', redirectTo:'users', pathMatch:'full'
+  },
+  {
+    path: "data-binding",
+    loadChildren: () =>
+      import('./modules/data-binding/data-binding.module').then((m) => m.DataBindingModule)
+  },
+  {
+    path: "directive-pipes",
+    loadChildren: () =>
+      import('./modules/directives-pipes/directives-pipes.module').then((m) => m.DirectivesPipesModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./modules/profile/profile.module').then(
+        (m) => m.ProfileModule
+      ),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./modules/users/users.module').then(
+        (m) => m.UsersModule
+      ),
+  },
+  {
+    path: 'resumebuilder',
+    loadChildren: () =>
+      import('./modules/resume-builder/resume-builder.module').then(
+        (m) => m.ResumeBuilderModule
+      ),
+  },
+  { path: 'employees', loadChildren: () => import('./Assesments/employees/employees.module').then(m => m.EmployeesModule) },
+  { path: 'practice', loadChildren: () => import('./modules/practice/practice.module').then(m => m.PracticeModule) },
+  { path: 'todo-app', loadChildren: () => import('./modules/todo/todo.module').then(m => m.TodoModule) },
+
+  { path: 'ngTemp', component:NgtemplateDemoComponent},
+  
+  { path: 'Temp-outlet',
+   loadChildren: () => import('./modules/demo-ng-template-outlet/demo-ng-template-outlet.module').then(
+     m => m.DemoNgTemplateOutletModule) } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
